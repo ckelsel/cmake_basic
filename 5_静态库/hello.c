0f0568b9b6f99cc10eb8c451b02c394a062c07c1
xiekunming <xm_gcc@163.com>
2020-08-15 16:14:44 +0800

    5

--- 5_静态库/hello.c
+++ 5_静态库/hello.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+
+#include "math.h"
+
 int main()
 {
-    printf("hello\n");
+    printf("hello, 1 + 1 = %d\n", add(1, 1));
     return 0;
 }
---
+++ 5_静态库/math.h
@@ -0,0 +1,8 @@
+#ifndef __MATH_H__
+#define __MATH_H__
+
+
+int add(int m, int n);
+
+
+#endif // __MATH_H__
